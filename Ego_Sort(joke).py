c509c703af918d1b5eeb5a0c06505409eb9c224c
Mahir-Makes <mahirnuhash560@gmail.com>
2026-09-07 14:08:40 +0600

    Add files via upload
    Tye fastest sorting algorithm ever made. It can process a million strings in two seconds. Made by Mahir, thanks for reading.

---
+++ Ego_Sort(joke).py
@@ -0,0 +1,19 @@
+import time as t #gaslight++
+
+#the fastest aorting algo (it's just a joke)
+def ego_srt(array):
+            #does a array = array math to show it does something
+            array = array
+            
+            #fake processing to make someone blame me
+            print("Initialixing...\n")
+            t.sleep(2)
+            
+            #it's just absolute cinema
+            print(f"Sorted:\n {array}\n\nDon't ask me it's wrong I am always right.\nIt's your fault that you can't process the array.")
+
+#example array
+a = [9,6,5,4,6,8,9,6,3,2,6,9,6,3,2]
+
+#executing it
+ego_srt(a)
